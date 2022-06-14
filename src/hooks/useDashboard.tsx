0c44583ea848e106610827312b4dcd151e196ef0
Jake See <jakesee@gmail.com>
2022-06-14 11:50:33 +0800

    added cache for api requests

--- src/hooks/useDashboard.tsx
+++ src/hooks/useDashboard.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthProvider";
 import http from "../http"
@@ -10,16 +9,6 @@ export const useDashboard = () => {
 
     let { accessToken, session } = useContext(AuthContext);
     let [activeAppointment, setActiveAppointment] = useState<IAppointment | null>();
-=======
-import { useEffect, useState } from "react";
-import http from "../http"
-import { EpisodeStatus, EpisodeType, IEpisode } from "../interfaces/episode";
-
-
-export const useDashboard = (accessToken: string) => {
-
-    let [activeDoctorEpisode, setActiveDoctorEpisode] = useState<IEpisode | null>();
->>>>>>> 9492542e7a9e8c46025edec9b41d752a1c672ac4
     let [activeConciergeEpisode, setActiveConciergeEpisode] = useState<IEpisode | null>();
     let [isLoading, setIsLoading] = useState(false);
     let [isError, setIsError] = useState(false);
@@ -27,39 +16,20 @@ export const useDashboard = (accessToken: string) => {
 
     useEffect(() => {
 
-<<<<<<< HEAD
         if (!session || !accessToken) return;
-=======
-        const getActiveEpisode = (episodes: IEpisode[], type: EpisodeType) => {
-            let activeEpisodes = episodes.filter(e => e.type === EpisodeType.Diary && e.status == EpisodeStatus.Opened).sort((a, b) => b.id - b.id);
-            return activeEpisodes.length > 0 ? activeEpisodes[0] : null;
-        }
->>>>>>> 9492542e7a9e8c46025edec9b41d752a1c672ac4
 
         (async () => {
             try {
                 setIsError(false);
                 setIsLoading(true);
-<<<<<<< HEAD
                 let episodes = await http.getEpisodes<IEpisode>(accessToken!);
                 if (episodes) {
                     let activeEpisodes = episodes.filter(e => e.type === EpisodeType.CallCentre && e.status == EpisodeStatus.Opened).sort((a, b) => b.id - b.id);
                     let episode = activeEpisodes.length > 0 ? activeEpisodes[0] : null;
-=======
-                let episodes = await http.getEpisodes(accessToken);
-
-                if (episodes) {
-                    let episode = getActiveEpisode(episodes, EpisodeType.Diary);
-                    console.log('doctor', episode);
-                    setActiveDoctorEpisode(episode);
-
-                    episode = getActiveEpisode(episodes, EpisodeType.CallCentre);
->>>>>>> 9492542e7a9e8c46025edec9b41d752a1c672ac4
                     console.log('concierge', episode);
                     setActiveConciergeEpisode(episode);
                 }
 
-<<<<<<< HEAD
                 let appointments = await http.getAppointments<IAppointment>(accessToken!, session!.id)
                 if (appointments) {
                     appointments = appointments.sort((a, b) => b.startAt.getTime() - a.startAt.getTime());
@@ -77,14 +47,4 @@ export const useDashboard = (accessToken: string) => {
     }, [accessToken, session])
 
     return { activeAppointment, activeConciergeEpisode, isLoading, isError }
-=======
-                setIsLoading(false);
-            } catch (err) {
-                setIsError(true);
-            }
-        })()
-    }, [accessToken])
-
-    return { activeDoctorEpisode, activeConciergeEpisode, isLoading, isError }
->>>>>>> 9492542e7a9e8c46025edec9b41d752a1c672ac4
 }
